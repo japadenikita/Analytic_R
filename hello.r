32fcafe7784eb8eda5d1485418dcd8797b0de9aa
japadenikita <68785812+japadenikita@users.noreply.github.com>
2020-07-26 17:13:30 +0530

    Add files via upload
    basic operation of r

---
+++ hello.r
@@ -0,0 +1,19 @@
+#numeric
+vec_num <- c(10,20,30)
+vec_num
+vec_num1 <- c(20,20,20)
+#add the two vector
+sum_vec <- vec_num1 + vec_num
+#sum_total
+sum_vec
+#slice the vector
+slice_vector <- c(1,2,3,4,5,6)
+slice_vector[1:3]
+#logical vector
+log_vec <- c(1:10)
+log_vec>5
+log_vec[(log_vec>5)]
+log_vec[(log_vec>2) & (log_vec<8)]
+#acess number
+x=seq(1,10,by=2)
+x <- (-3:2)
